23081813c0fd5927e8cbc74cb64af719b343e21d
joeys1122 <josephs1122@gmail.com>
2022-02-25 12:29:53 -0500

    restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+
+  if(!token) {
+    next({ status: 401, message: 'token required' })
+  } else {
+    jwt.verify(token, process.env.SECRET || 'adsfdasfkljsd', (err, decodedToken) => {
+      if(err) {
+        next({ status: 401, message: 'token invalid' });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
+  }
+  
   /*
     IMPLEMENT
 
